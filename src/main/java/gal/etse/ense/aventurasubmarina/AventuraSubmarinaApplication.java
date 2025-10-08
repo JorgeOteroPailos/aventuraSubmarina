d6d71a03a581ffbbe2f63ce484233aa0f869cde0
Jorge Otero Pailos <jorgeoteropailos@gmail.com>
2025-10-08 09:42:27 +0200

    inicialización

---
+++ src/main/java/gal/etse/ense/aventurasubmarina/AventuraSubmarinaApplication.java
@@ -0,0 +1,12 @@
+package gal.etse.ense.aventurasubmarina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AventuraSubmarinaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AventuraSubmarinaApplication.class, args);
+    }
+
+}
